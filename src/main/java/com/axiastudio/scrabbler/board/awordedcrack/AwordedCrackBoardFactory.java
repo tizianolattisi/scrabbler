4d9cd3ad1ed1bba27c2b17e79fbe11721d991b48
Tiziano Lattisi <tiziano@axiastudio.it>
2019-01-24 22:19:19 +0100

    AwordedBoard completed, and test for symmetricity.

--- src/main/java/com/axiastudio/scrabbler/board/awordedcrack/AwordedCrackBoardFactory.java
+++ src/main/java/com/axiastudio/scrabbler/board/awordedcrack/AwordedCrackBoardFactory.java
@@ -13,7 +13,7 @@ public class AwordedCrackBoardFactory implements BoardFactory {
     public Board buildAndInitialize() {
         AwordedCrackBoard board = new AwordedCrackBoard(15);
 
-        board.setSquare(new Position(1, 0), new Square(3, LetterOrWord.WORD));
+        board.setSquare(new Position(2, 0), new Square(3, LetterOrWord.WORD));
         board.setSquare(new Position(4, 0), new Square(3, LetterOrWord.LETTER));
         board.setSquare(new Position(10, 0), new Square(3, LetterOrWord.LETTER));
         board.setSquare(new Position(12, 0), new Square(3, LetterOrWord.WORD));
@@ -30,8 +30,64 @@ public class AwordedCrackBoardFactory implements BoardFactory {
         board.setSquare(new Position(12, 2), new Square(2, LetterOrWord.LETTER));
         board.setSquare(new Position(14, 2), new Square(3, LetterOrWord.WORD));
 
-        // TODO: complete aworded crack board layout
-        
+        board.setSquare(new Position(3, 3), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(7, 3), new Square(2, LetterOrWord.WORD));
+        board.setSquare(new Position(11, 3), new Square(3, LetterOrWord.LETTER));
+
+        board.setSquare(new Position(0, 4), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(6, 4), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(8, 4), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(14, 4), new Square(3, LetterOrWord.LETTER));
+
+        board.setSquare(new Position(1, 5), new Square(2, LetterOrWord.WORD));
+        board.setSquare(new Position(5, 5), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(9, 5), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(13, 5), new Square(2, LetterOrWord.WORD));
+
+        board.setSquare(new Position(2, 6), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(4, 6), new Square(2, LetterOrWord.LETTER));
+        board.setSquare(new Position(10, 6), new Square(2, LetterOrWord.LETTER));
+        board.setSquare(new Position(12, 6), new Square(3, LetterOrWord.LETTER));
+
+        board.setSquare(new Position(3, 7), new Square(2, LetterOrWord.WORD));
+        board.setSquare(new Position(11, 7), new Square(2, LetterOrWord.WORD));
+
+        board.setSquare(new Position(2, 8), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(4, 8), new Square(2, LetterOrWord.LETTER));
+        board.setSquare(new Position(10, 8), new Square(2, LetterOrWord.LETTER));
+        board.setSquare(new Position(12, 8), new Square(3, LetterOrWord.LETTER));
+
+        board.setSquare(new Position(1, 9), new Square(2, LetterOrWord.WORD));
+        board.setSquare(new Position(5, 9), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(9, 9), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(13, 9), new Square(2, LetterOrWord.WORD));
+
+        board.setSquare(new Position(0, 10), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(6, 10), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(8, 10), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(14, 10), new Square(3, LetterOrWord.LETTER));
+
+        board.setSquare(new Position(3, 11), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(7, 11), new Square(2, LetterOrWord.WORD));
+        board.setSquare(new Position(11, 11), new Square(3, LetterOrWord.LETTER));
+
+        board.setSquare(new Position(0, 12), new Square(3, LetterOrWord.WORD));
+        board.setSquare(new Position(2, 12), new Square(2, LetterOrWord.LETTER));
+        board.setSquare(new Position(6, 12), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(8, 12), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(12, 12), new Square(2, LetterOrWord.LETTER));
+        board.setSquare(new Position(14, 12), new Square(3, LetterOrWord.WORD));
+
+        board.setSquare(new Position(1, 13), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(5, 13), new Square(2, LetterOrWord.WORD));
+        board.setSquare(new Position(9, 13), new Square(2, LetterOrWord.WORD));
+        board.setSquare(new Position(13, 13), new Square(3, LetterOrWord.LETTER));
+
+        board.setSquare(new Position(2, 14), new Square(3, LetterOrWord.WORD));
+        board.setSquare(new Position(4, 14), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(10, 14), new Square(3, LetterOrWord.LETTER));
+        board.setSquare(new Position(12, 14), new Square(3, LetterOrWord.WORD));
+
         return board;
     }
 
--- src/test/java/com/axiastudio/scrabbler/TestAwordedCrackBoard.java
+++ src/test/java/com/axiastudio/scrabbler/TestAwordedCrackBoard.java
@@ -19,11 +19,30 @@ public class TestAwordedCrackBoard {
     }
 
     @Test
-    public void testAwordedCrackBoardCreation() {
+    public void testBoardCreation() {
         Square square = board.getSquare(new Position(1, 0));
         Assertions.assertTrue(square.isEmpty());
         Assertions.assertTrue(square.isMultiplicatorForWord());
         Assertions.assertEquals(3, (int) square.getMultiplicator());
+    }
+
+    @Test
+    public void testBoardIsSymmetric() {
+        int maxSizeToCheck = (board.size() + 1) / 2;
+        for (int i=0; i<maxSizeToCheck; i++) {
+            for (int j=0; j<maxSizeToCheck; j++) {
+                Square northEastSquare = board.getSquare(new Position(i, j));
+                Square northWestSquare = board.getSquare(new Position(board.size()-1-i, j));
+                Square southEastSquare = board.getSquare(new Position(i, board.size()-1-j));
+                Square southWestSquare = board.getSquare(new Position(board.size()-1-i, board.size()-1-j));
+                Assertions.assertEquals(northEastSquare.getMultiplicator(), northWestSquare.getMultiplicator());
+                Assertions.assertEquals(northWestSquare.getMultiplicator(), southEastSquare.getMultiplicator());
+                Assertions.assertEquals(southEastSquare.getMultiplicator(), southWestSquare.getMultiplicator());
+                Assertions.assertEquals(northEastSquare.isMultiplicatorForWord(), northWestSquare.isMultiplicatorForWord());
+                Assertions.assertEquals(northWestSquare.isMultiplicatorForWord(), southEastSquare.isMultiplicatorForWord());
+                Assertions.assertEquals(southEastSquare.isMultiplicatorForWord(), southWestSquare.isMultiplicatorForWord());
+            }
+        }
     }
 
 }
