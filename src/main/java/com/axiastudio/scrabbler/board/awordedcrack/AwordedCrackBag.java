2c4b3d626c7caa3969d8845a9d780ce8d167f8b1
Tiziano Lattisi <tiziano@axiastudio.it>
2019-01-24 21:43:27 +0100

    The AwordedCrack board uses the classic bag.

--- src/main/java/com/axiastudio/scrabbler/board/awordedcrack/AwordedCrackBag.java
+++
@@ -1,7 +0,0 @@
-package com.axiastudio.scrabbler.board.awordedcrack;
-
-import com.axiastudio.scrabbler.bag.AbstractBag;
-import com.axiastudio.scrabbler.core.Bag;
-
-public class AwordedCrackBag extends AbstractBag implements Bag {
-}
