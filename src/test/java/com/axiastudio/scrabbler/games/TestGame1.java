a37c4c5a9abea482eb1a93fa7b1f6d78198add61
Tiziano Lattisi <tiziano@axiastudio.it>
2019-01-29 17:56:35 +0100

    End game1.

--- src/test/java/com/axiastudio/scrabbler/games/TestGame1.java
+++ src/test/java/com/axiastudio/scrabbler/games/TestGame1.java
@@ -1,19 +1,13 @@
 package com.axiastudio.scrabbler.games;
 
-import com.axiastudio.scrabbler.core.Orientation;
-import com.axiastudio.scrabbler.core.Pattern;
-import com.axiastudio.scrabbler.core.Position;
 import com.axiastudio.scrabbler.customs.awordedcrack.AwordedCrackBoardFactory;
 import com.axiastudio.scrabbler.customs.classic.ClassicBagFactory;
 import com.axiastudio.scrabbler.customs.classic.ClassicDictionaryFactory;
 import com.axiastudio.scrabbler.engine.Engine;
 import com.axiastudio.scrabbler.engine.Solution;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /*
  *   This is a real game played again a real player (thank's LucaB)
@@ -165,7 +159,7 @@ public class TestGame1 {
         String[] lettersToPlace = {
                 "  acqua cuvee  ",
                 "     amba      ",
-                "      m        ",
+                "      m pruvaie",
                 "      i        ",
                 "     or        ",
                 "     ha        ",
@@ -180,10 +174,244 @@ public class TestGame1 {
                 " pe            "
         };
         engine.placeLetters(lettersToPlace);
-        String lettersInMyHand = "epuravi";
+        String lettersInMyHand = "rroltoi";
         Solution solution = engine.bestSolution(lettersInMyHand);
         System.out.println(solution);
-        // ?
+        // litio
     }
 
+    @Test
+    public void testRound7() {
+        // note: the "pruvaie" solution was a suggest to the opponent... ;-)
+        String[] lettersToPlace = {
+                "  acqua cuvee  ",
+                "     amba      ",
+                "mole  m pruvaie",
+                "   litio       ",
+                "     or        ",
+                "     ha        ",
+                "      t        ",
+                "   sazio       ",
+                "   b i         ",
+                "   a f         ",
+                " c v           ",
+                " ago           ",
+                " rh            ",
+                " pi            ",
+                " pe            "
+        };
+        engine.placeLetters(lettersToPlace);
+        String lettersInMyHand = "rroliad";
+        Solution solution = engine.bestSolution(lettersInMyHand);
+        System.out.println(solution);
+        // dolorai
+    }
+
+    @Test
+    public void testRound8() {
+        String[] lettersToPlace = {
+                "  acqua cuvee  ",
+                " d   amba      ",
+                "mole  m pruvaie",
+                " l litio       ",
+                " o   or        ",
+                " r   ha        ",
+                " a    t        ",
+                " i sazio       ",
+                "   b i cestino ",
+                "   a f         ",
+                " c v           ",
+                " ago           ",
+                " rh            ",
+                " pi            ",
+                " pe            "
+        };
+        engine.placeLetters(lettersToPlace);
+        String lettersInMyHand = "rraseea";
+        Solution solution = engine.bestSolution(lettersInMyHand);
+        System.out.println(solution);
+        // astraere
+    }
+
+    @Test
+    public void testRound9() {
+        String[] lettersToPlace = {
+                "  acqua cuvee  ",
+                " d   amba      ",
+                "mole  m pruvaie",
+                " l litio       ",
+                "go   or        ",
+                "or   ha        ",
+                "ta    t   a    ",
+                "oi sazio  s    ",
+                "   b i cestino ",
+                "   a f    r    ",
+                " c v      a    ",
+                " ago      e    ",
+                " rh       r    ",
+                " pi       e    ",
+                " pe            "
+        };
+        engine.placeLetters(lettersToPlace);
+        String lettersInMyHand = "ffeltcp";
+        Solution solution = engine.bestSolution(lettersInMyHand);
+        System.out.println(solution);
+        // felpe
+    }
+
+    @Test
+    public void testRound10() {
+        String[] lettersToPlace = {
+                "  acqua cuvee  ",
+                " d   amba      ",
+                "mole  m pruvaie",
+                " l litio       ",
+                "go   or        ",
+                "or   ha        ",
+                "ta    t   a    ",
+                "oi sazio  s    ",
+                "   b i cestino ",
+                "   a f    r    ",
+                " c v      a    ",
+                " ago      e    ",
+                " rh       r    ",
+                " pi      felpe ",
+                " pe         cab"
+        };
+        engine.placeLetters(lettersToPlace);
+        String lettersInMyHand = "fnuotcm";
+        Solution solution = engine.bestSolution(lettersInMyHand);
+        System.out.println(solution);
+        // futon
+    }
+
+    @Test
+    public void testRound11() {
+        String[] lettersToPlace = {
+                "  acqua cuvee  ",
+                " d   amba      ",
+                "mole  m pruvaie",
+                " l litio       ",
+                "go   or        ",
+                "or   ha      f ",
+                "ta    t   a  u ",
+                "oi sazio  s  t ",
+                "   b i cestino ",
+                "   a f    r  ni",
+                " c v      a   n",
+                " ago      e   n",
+                " rh       r   o",
+                " pi      felpe ",
+                " pe         cab"
+        };
+        engine.placeLetters(lettersToPlace);
+        String lettersInMyHand = "doeoocm";
+        Solution solution = engine.bestSolution(lettersInMyHand);
+        System.out.println(solution);
+        // comode
+    }
+
+    @Test
+    public void testRound12() {
+        String[] lettersToPlace = {
+                "  acqua cuvee  ",
+                " d   amba      ",
+                "mole  m pruvaie",
+                " l litio       ",
+                "go   or        ",
+                "or   ha      f ",
+                "ta    t   a zum",
+                "oi sazio  s  t ",
+                "   b i cestino ",
+                "   a f    r  ni",
+                " c v      a   n",
+                " ago comode   n",
+                " rh       r   o",
+                " pi      felpe ",
+                " pe         cab"
+        };
+        engine.placeLetters(lettersToPlace);
+        String lettersInMyHand = "nessuso";
+        Solution solution = engine.bestSolution(lettersInMyHand);
+        System.out.println(solution);
+        // cune
+    }
+
+    @Test
+    public void testRound13() {
+        String[] lettersToPlace = {
+                "  acqua cuvee  ",
+                " d   amba      ",
+                "mole  m pruvaie",
+                " l litio       ",
+                "go   or        ",
+                "or   ha      f ",
+                "ta    t   a zum",
+                "oi sazio  s  t ",
+                "   b i cestino ",
+                "   a f    r  ni",
+                " c v      a   n",
+                " ago comode   n",
+                " rh  u    r   o",
+                " pi  n   felpe ",
+                " pe  e ido  cab"
+        };
+        engine.placeLetters(lettersToPlace);
+        String lettersInMyHand = "aoessso";
+        Solution solution = engine.bestSolution(lettersInMyHand);
+        System.out.println(solution);
+        // sesso
+    }
+
+    @Test
+    public void testRound14() {
+        String[] lettersToPlace = {
+                "  acqua cuvee s",
+                " d   amba     t",
+                "mole  m pruvaie",
+                " l litio      l",
+                "go   or       i",
+                "or   ha  s   f ",
+                "ta    t  ea zum",
+                "oi sazio ss  t ",
+                "   b i cestino ",
+                "   a f   or  ni",
+                " c v      a   n",
+                " ago comode   n",
+                " rh  u    r   o",
+                " pi  n   felpe ",
+                " pe  e ido  cab"
+        };
+        engine.placeLetters(lettersToPlace);
+        String lettersInMyHand = "aos";
+        Solution solution = engine.bestSolution(lettersInMyHand);
+        System.out.println(solution);
+        // zano
+    }
+
+    @Test
+    public void testRound15() {
+        String[] lettersToPlace = {
+                "  acqua cuvee s",
+                " d   amba     t",
+                "mole  m pruvaie",
+                " l litio      l",
+                "go   or       i",
+                "or   ha  s   f ",
+                "tai   t  ea zum",
+                "oi sazio ss  t ",
+                "   b i cestino ",
+                "   a f   or  ni",
+                " c v      a   n",
+                " ago comode   n",
+                " rh  u    r   o",
+                " pi  n   felpe ",
+                " pe  e ido  cab"
+        };
+        engine.placeLetters(lettersToPlace);
+        String lettersInMyHand = "s";
+        Solution solution = engine.bestSolution(lettersInMyHand);
+        System.out.println(solution);
+        // the opponent is the winner (but with a hint in round 7) 628 - 692
+    }
 }
